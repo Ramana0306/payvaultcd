876e5a94d2679757c46e9a6e65110a81ac73d1a1
Ramana0306 <m.ramanamurugan2006@gmail.com>
2026-06-20 20:06:12 +0530

    Update

--- src/App.js
+++ src/App.js
@@ -234,7 +234,7 @@ function App() {
                 <div className="inventor-avatar">R</div>
                 <div className="inventor-info">
                   <h4>Ramana Murugan</h4>
-                  <span>Inventor & Developer</span>
+                  <span>Inventor</span>
                   <span className="inventor-degree">B.Tech Information Technology</span>
                 </div>
               </div>
